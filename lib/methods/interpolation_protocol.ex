ae7dc41d13b5aaff9a7a4d61f3b97b20a1fbddf7
jaba <jabannishe@gmail.com>
2024-12-14 20:02:08 +0300

    fix of broken test

--- lib/methods/interpolation_protocol.ex
+++ lib/methods/interpolation_protocol.ex
@@ -6,9 +6,6 @@ defprotocol Methods.InterpolationProtocol do
     return list of points, list consist of points starting from started_point.x and stepping it
     until it less than finishing_point.x. For each x is interpolating the value of y
 
-    ### Example:
-      iex> Methods.Linear.apply(1.5, [%Point{x: 1.3, y: 4}, %Point{x: 5, y: 7}])
-      [%Point{x: 1.3, y: 4}, %Point{x: 2.8, y: 5.216216216216216}, %Point{x: 4.3, y: 6.4324324324324325}]
   """
   @spec apply(method :: atom(), step :: float(), points :: list(Point.t())) ::
           list(Point.t()) | {:error, String.t()}
